60b5dcf03ec3f5a67c878af0cd1add19ff3892c3
Gardenia Oliveira <jobs.gardeniaoliveira@gmail.com>
2023-03-24 21:41:28 +0000

    created helpers with functions to filter month and year in the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { useState, useEffect } from "react";
 import * as C from "./App.styles";
+import { Item } from "./types/Item";
+import { Category } from "./types/Category";
+import { categories } from "./data/categories";
+import { items } from "./data/items";
+import * as Fun from "./helpers/dateFilter";
 
 const App = () => {
+  // general list of all months
+  const [list, setList] = useState(items);
+
+  // list filtered for each months
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMont] = useState(Fun.getCurrentMonth());
+
+  //monitors when changes the month and execute the function
+  useEffect(() => {
+    setFilteredList(Fun.filterListByMonth(list, currentMonth));
+  }, [list, currentMonth]);
   return (
     <C.Container>
       <C.Header>
---
+++ src/helpers/dateFilter.ts
@@ -0,0 +1,27 @@
+import { Item } from "../types/Item";
+
+export const getCurrentMonth = () => {
+  let now = new Date();
+  return `${now.getFullYear()} ${now.getMonth() + 1}`;
+};
+
+//filter the old list and create a new list with new dates
+export const filterListByMonth = (list: Item[], date: string): Item[] => {
+  let newList: Item[] = [];
+
+  //separate the - between month and year (2023-3)
+  let [year, month] = date.split("-");
+
+  //compare if the year and month requested are the same from the list
+  for (let i in list) {
+    if (
+      list[i].date.getFullYear() === parseInt(year) &&
+      list[i].date.getMonth() + 1 === parseInt(month)
+    ) {
+      // if the year and month requested are the same from the list, so push to the new list
+      newList.push(list[i]);
+    }
+  }
+  // return a list with items only the same month and year
+  return newList;
+};
